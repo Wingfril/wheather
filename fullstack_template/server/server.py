dc542b4a4059eefe62780e14f60494eed27a180a
Wingfril <moziyan@yahoo.com>
2017-12-02 02:48:27 -0800

    frontpage
    Started creating the front page;
    created get url in python

--- fullstack_template/server/server.py
+++ fullstack_template/server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
@@ -6,9 +7,17 @@ app = Flask(__name__, static_folder="../static/dist", template_folder="../static
 def index():
     return render_template("index.html")
 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
+@app.route("/main")
+def submitted():
+    return render_template("index.html")
+
+# Get the jsoned weather data
+def get(url):
+    try:
+        res = requests.get(url)
+        return res.json()
+    except:
+        return False
 
 if __name__ == "__main__":
 	app.run()
--- fullstack_template/static/js/App.jsx
+++ fullstack_template/static/js/App.jsx
@@ -2,6 +2,15 @@ import React from "react";
 
 export default class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
-  }
+    return (
+<div>
+    <h1>wheather</h1>
+    <p>Hello!</p>
+    <form>
+        <input type="text" name="number" placeholder="phone number" />
+        <input type="submit" value="submit" />
+    </form>
+</div>
+    );
+    }
 }
