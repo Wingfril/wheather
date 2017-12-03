d1957d6d3f6d78f9a93c12ceceb232ad7f45bf21
Shawna Huang <shawna_huang@brown.edu>
2017-12-02 22:43:36 -0500

    css now works!

--- fullstack_template/static/js/App.jsx
+++ fullstack_template/static/js/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Form from "./Form";
-// import '../main.css';
+require('../main.css');
 
 class App extends React.Component {
   render () {
--- fullstack_template/static/webpack.config.js
+++ fullstack_template/static/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
 	entry: __dirname + '/js/index.jsx',
@@ -16,13 +17,23 @@ const config = {
 	    exclude: /node_modules/,
 	    use: 'babel-loader'
 	    },
-	    // {      
-     //    test: /\.css$/,
-     //    use: [ 'style-loader', 'css-loader' ]
-     //  }
+		{
+		  test: /\.css$/,
+		  use: ExtractTextPlugin.extract({
+		         fallback: 'style-loader',
+		         use: 'css-loader',
+		       })
+		},
+		{		
+		  test: /\.(png|svg|jpg|gif)$/,
+		  use: 'file-loader'
+		}
 	  ]
-	}
-}
+	},
+	plugins: [  
+		new ExtractTextPlugin('styles.css')
+		]
+};
 
 
 module.exports = config;
